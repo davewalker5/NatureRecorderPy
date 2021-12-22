d1b68b076c8d4d459f6fc6ed104d9e005abe035b
David Walker <david.walker5@icloud.com>
2021-12-22 16:30:09 +0000

    Add ability to relocate the data folder via an environment variable

--- src/naturerec_model/model/utils.py
+++ src/naturerec_model/model/utils.py
@@ -11,8 +11,10 @@ def get_data_path():
 
     :return: The path to the data folder
     """
-    project_folder = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-    data_folder = os.path.join(project_folder, "data")
-    if not os.path.exists(data_folder):
-        os.makedirs(data_folder)
+    data_folder = os.environ["NATURE_RECORDER_DATA_FOLDER"] if "NATURE_RECORDER_DATA_FOLDER" in os.environ else None
+    if not data_folder:
+        project_folder = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+        data_folder = os.path.join(project_folder, "data")
+        if not os.path.exists(data_folder):
+            os.makedirs(data_folder)
     return data_folder
