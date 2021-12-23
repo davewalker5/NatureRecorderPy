ea160c0594ec0eb5e50c0167b3d8f78a77b04969
David Walker <david.walker5@icloud.com>
2021-12-23 04:19:40 +0000

    Added the ability to add new conservation status ratings to a scheme

--- src/naturerec_web/status/status_blueprint.py
+++ src/naturerec_web/status/status_blueprint.py
@@ -4,6 +4,7 @@ The status blueprint supplies view functions and templates for conservation stat
 
 from flask import Blueprint, render_template, request, redirect
 from naturerec_model.logic import list_status_schemes, get_status_scheme, create_status_scheme, update_status_scheme
+from naturerec_model.logic import create_status_rating
 
 
 status_bp = Blueprint("status", __name__, template_folder='templates')
@@ -24,6 +25,25 @@ def _render_status_scheme_editing_page(status_scheme_id, error):
                            error=error)
 
 
+def _render_status_rating_editing_page(status_scheme_id, status_rating_id, error):
+    """
+    Helper to render the consevation status rating editing page
+
+    :param status_scheme_id: ID for the parent conservation status scheme
+    :param status_rating_id: ID for the conservation status rating to edit or None for addition
+    :param error: Error message to display on the page or None
+    :return: The rendered editing template
+    """
+    status_scheme = get_status_scheme(status_scheme_id) if status_scheme_id else None
+    matching_ratings = [rating for rating in status_scheme.ratings if rating.id == status_rating_id]
+    status_rating = matching_ratings[0] if matching_ratings else None
+    return render_template("status/edit_rating.html",
+                           status_scheme_id=status_scheme.id if status_scheme else None,
+                           status_rating=status_rating,
+                           edit_enabled=True,
+                           error=error)
+
+
 @status_bp.route("/list")
 def list_all():
     """
@@ -37,10 +57,10 @@ def list_all():
 
 
 @status_bp.route("/edit", defaults={"status_scheme_id": None}, methods=["GET", "POST"])
-@status_bp.route("/add/<int:status_scheme_id>", methods=["GET", "POST"])
+@status_bp.route("/edit/<int:status_scheme_id>", methods=["GET", "POST"])
 def edit_scheme(status_scheme_id):
     """
-    Serve the page to add new species or edit an existing one and handle the appropriate action
+    Serve the page to add new conservation status rating or edit an existing one and handle the appropriate action
     when the form is submitted
 
     :param status_scheme_id: ID for a conservation status scheme to edit or None to create a new one
@@ -57,3 +77,27 @@ def edit_scheme(status_scheme_id):
             return _render_status_scheme_editing_page(status_scheme_id, e)
     else:
         return _render_status_scheme_editing_page(status_scheme_id, None)
+
+
+@status_bp.route("/add_rating/<int:status_scheme_id>", defaults={"status_rating_id": None}, methods=["GET", "POST"])
+@status_bp.route("/edit_rating/<int:status_scheme_id>/<int:status_rating_id>", methods=["GET", "POST"])
+def edit_rating(status_scheme_id, status_rating_id):
+    """
+    Serve the page to add new species or edit an existing one and handle the appropriate action
+    when the form is submitted
+
+    :param status_scheme_id: ID for the parent conservation status scheme
+    :param status_rating_id: ID for a conservation status rating to edit or None to create a new one
+    :return: The HTML for the data entry page or a response object redirecting to the scheme list page
+    """
+    if request.method == "POST":
+        try:
+            if status_rating_id:
+                pass
+            else:
+                _ = create_status_rating(status_scheme_id, request.form["name"])
+            return redirect(f"/status/edit/{status_scheme_id}")
+        except ValueError as e:
+            return _render_status_rating_editing_page(status_scheme_id, status_rating_id, e)
+    else:
+        return _render_status_rating_editing_page(status_scheme_id, status_rating_id, None)
