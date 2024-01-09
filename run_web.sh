eddd80be53a1712ee470391dfa3dec4c1d2c5cf6
Dave Walker <david.walker5@icloud.com>
2024-01-09 19:10:16 +0000

    Fixup vulnerable fonttools dependency

--- run_web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh -f
-
-export PROJECT_ROOT=$( cd "$(dirname "$0")" ; pwd -P )
-source "$PROJECT_ROOT/venv/bin/activate"
-export PYTHONPATH="$PROJECT_ROOT/src"
-export NATURE_RECORDER_DB="$PROJECT_ROOT/data/naturerecorder.db"
-export FLASK_ENV=development
-python -m naturerec_web
