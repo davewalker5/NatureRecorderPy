2adc530c5bb1a33151c573d7986d18346b1286f4
Dave Walker <david.walker5@icloud.com>
2025-04-19 09:44:15 +0100

    Split out documentation build requirements

--- docs/make_docs.sh
+++ docs/make_docs.sh
@@ -1,8 +1,18 @@
-#!/bin/zsh -f
+#!/bin/bash -f
 
-source ../venv/bin/activate
-PROJECTDIR=${0:a:h}/..
-export PYTHONPATH="$PROJECTDIR/src:$PROJECTDIR/tests"
-echo "Python path is set to: $PYTHONPATH"
+DOCS_FOLDER="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+PROJECT_ROOT="$DOCS_FOLDER/.."
+export PYTHONPATH="$PROJECT_ROOT/src"
+
+echo "Documents Folder : $DOCS_FOLDER"
+echo "Project Root     : $PROJECT_ROOT"
+echo "Python Path      : $PYTHONPATH"
+
+# The assumption is there is already a virtual environment with the requirements to
+# run the analyser installed. This simply activates it and adds in the Sphinx requirements
+. "$PROJECT_ROOT/venv/bin/activate"
+pip install -r requirements.txt
+
+# Build the documentation
 make html
 deactivate
--- docs/source/locust_tests/index.rst
+++
@@ -1,8 +0,0 @@
-Locust Load and Performance Tests
-=================================
-
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
-
-   locustfile
